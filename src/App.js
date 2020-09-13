903b44713c61face3e646f39621909b3de3dc642
Raphael <raphael.haehnel@gmail.com>
2020-09-13 14:49:12 +0300

    lesson19

--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import './App.css';
 
 class App extends Component {
-  render() {
-    const name = 'Raphael Haehnel';
-    const foo = () => 'Bar';
+  state = {
+    users: [],
+    loading: false,
+    alert: null,
+  };
 
-    const activateLasers = () => (name = 'You have just been hacked');
+  /*  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=$
+      {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+      {process.env.REACT_APP_GITHUB_CLIENT_ID}`
+    );
+    this.setState({ users: res.data, loading: false });
+  } */
+
+  // Search Github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=$
+      {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+      {process.env.REACT_APP_GITHUB_CLIENT_ID}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
+  render() {
+    const { users, loading } = this.state;
+
     return (
-      <div className='App'>
-        <Navbar /> {/*There are two default props: title and icon*/}
-        <UserItem />
-      </div>
+      <Router>
+        <div className='App'>
+          <Navbar /> {/*There are two default props: title and icon*/}
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,26 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-  static defaultProps = {
-    title: 'Github Finder',
-    icon: 'fab fa-github',
-  };
+//We are not using the <a> tag for the navigation bar, because links clear the state. And we don't want to clear the state each time
+import { Link } from 'react-router-dom';
 
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
+const Navbar = ({ icon, title }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>
+        <li>
+          <Link to='/'> Home </Link>
+        </li>
+        <li>
+          <Link to='/about'> About </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <i className={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github',
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 
 export default Navbar;
